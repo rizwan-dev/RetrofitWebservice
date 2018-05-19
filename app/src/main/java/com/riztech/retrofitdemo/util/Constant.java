c0d40059584d9408b57781d1526d2965a296a535
Rizwanul Haque <rizwan@relfor.com>
2018-05-19 12:50:11 +0530

    Added URL

---
+++ app/src/main/java/com/riztech/retrofitdemo/util/Constant.java
@@ -0,0 +1,5 @@
+package com.riztech.retrofitdemo.util;
+
+public class Constant {
+    public static final String MOVIE_URL = "http://api.themoviedb.org/3/movie/top_rated?api_key=7e8f60e325cd06e164799af1e317d7a7";
+}
